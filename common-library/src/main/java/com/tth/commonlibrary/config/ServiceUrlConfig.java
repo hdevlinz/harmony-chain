2a44b2cec6256bc472e68607056769e3bc35f52c
Thanh Hiep <hieptt.2003@gmail.com>
2024-10-02 12:21:02 +0700

    FIX
    Signed-off-by: Thanh Hiep <hieptt.2003@gmail.com>

--- common-library/src/main/java/com/tth/commonlibrary/config/ServiceUrlConfig.java
+++
@@ -1,7 +0,0 @@
-package com.tth.commonlibrary.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "tth.services")
-public record ServiceUrlConfig(String media, String product) {
-}
