57af46199e8f0ccc1ab2de68e7fe79e4b9aacdbb
Thanh Hiep <hieptt.2003@gmail.com>
2024-10-13 17:02:36 +0700

    refactor: 📝 update GitHub Actions workflows

--- workflows.sh
+++ workflows.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yaml)
-SERVICES=(gateway cart file identity inventory notification order product profile rating shipment) # Add your services here
+SERVICES=(cart file identity inventory notification order product profile rating shipment) # Add your services here
 
 for SERVICE_NAME in "${SERVICES[@]}"; do
     echo "Generating workflow for ${SERVICE_NAME} service"
 
     WORKFLOW="${WORKFLOW_TEMPLATE//\{\{SERVICE_NAME\}\}/${SERVICE_NAME}}"
     echo "${WORKFLOW}" > ".github/workflows/${SERVICE_NAME}-ci.yaml"
+done
+
+WORKFLOW_TEMPLATE2=$(cat .github/workflow-template2.yaml)
+SERVICES2=(api-gateway) # Add your services here
+
+for SERVICE_NAME2 in "${SERVICES2[@]}"; do
+    echo "Generating workflow for ${SERVICE_NAME2} service"
+
+    WORKFLOW2="${WORKFLOW_TEMPLATE2//\{\{SERVICE_NAME\}\}/${SERVICE_NAME2}}"
+    echo "${WORKFLOW2}" > ".github/workflows/${SERVICE_NAME2}-ci.yaml"
 done
